5f4ac983d01b810f68641fb1466bf2d060f7db29
Charlie Fang <Charlie@rescomp-10-151544.Stanford.EDU>
2011-05-01 23:29:50 -0700

    Fixed some bugs with search engine stuff.

--- box/suggestion_engine/textextract.py
+++ box/suggestion_engine/textextract.py
@@ -4,7 +4,7 @@ from box import constants
 import subprocess
 
 def extract_text(filename):
-    args = constants.POI_SUBPROCESS_ARGS
+    args = list(constants.POI_SUBPROCESS_ARGS)
     args.append(filename)
     pipe = subprocess.Popen(args, stdout=subprocess.PIPE)
     return pipe.stdout.read().decode('utf-8', 'ignore')
